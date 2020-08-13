221d7cedc1eb42a9b2e12090044c7e5f12dec1c9
David Ianakiara <david.ianakiara@gmail.com>
2020-08-13 16:03:02 -0700

    Delete hitbtc_market_keeper.sh

--- hitbtc_market_keeper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-bin/hitbtc-market-maker-keeper --hitbtc-api-key "hKW475sHiQBp7dvbHt-L-9MXO8-_3bGo" \
---hitbtc-secret-key "hdGGjwa9lUFqP-JZmbRkb_C5E3iF5TBD" \
---pair "KINETH" --config bands.json --price-feed hitbtc --debug
-
